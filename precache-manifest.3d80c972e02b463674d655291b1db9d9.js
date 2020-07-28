b7be72ef7578178e9bada85c334b66a193585229
vera <poletaeva.vera.vladimirovna@gmail.com>
2020-07-28 18:35:23 +0300

    Updates

---
+++ precache-manifest.3d80c972e02b463674d655291b1db9d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b86085b60e6d6b815fabc869cef15d",
+    "url": "/VeraPoletaeva87/Vera.Poletaeva87.github.io/index.html"
+  },
+  {
+    "revision": "49fa92003995fd01c91b",
+    "url": "/VeraPoletaeva87/Vera.Poletaeva87.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b965d82a7be022cee79",
+    "url": "/VeraPoletaeva87/Vera.Poletaeva87.github.io/static/js/2.a9922afc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VeraPoletaeva87/Vera.Poletaeva87.github.io/static/js/2.a9922afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fa92003995fd01c91b",
+    "url": "/VeraPoletaeva87/Vera.Poletaeva87.github.io/static/js/main.62ad1aff.chunk.js"
+  },
+  {
+    "revision": "e8b81c46bcfe91d019c4",
+    "url": "/VeraPoletaeva87/Vera.Poletaeva87.github.io/static/js/runtime-main.0d8f97f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VeraPoletaeva87/Vera.Poletaeva87.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
