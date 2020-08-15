ebe1b0cea2e615c06997a10a049e8ca63a4434ae
vera <poletaeva.vera.vladimirovna@gmail.com>
2020-08-15 18:58:31 +0300

    feat: const birds feat: lat name random bird selection fix: audio change

---
+++ src/components/App.jsx
@@ -0,0 +1,171 @@
+import React from 'react';
+import logo from '../assets/title.png';
+import unknownBird from '../assets/unknownBird.png';
+import congrats from '../assets/congrats.png';
+import rightMP3 from '../assets/win31.mp3';
+import wrongMP3 from '../assets/wrong_answer.mp3';
+import './App.css';
+import { getBirdsByStep, getInitialState, getRandomBirdByStep } from './App.helpers';
+import { maxScore, steps } from './App.constants';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = getInitialState();
+    this.rightAudio = React.createRef();
+    this.wrongAudio = React.createRef();
+    this.questionAudio = React.createRef();
+  }
+  
+  onClick = () => {
+    const { currentStep } = this.state;
+    if (currentStep < 5) {
+      const nextStep = currentStep + 1;
+      this.setState({
+        currentBird: getRandomBirdByStep(nextStep),
+        currentStep: nextStep,
+        clickedBirds: [],
+        guessed: false
+      });
+    } else {
+      this.setState({
+        finished: true
+      });
+    }
+  };
+  
+  repeatClick = () => {
+    this.setState(getInitialState());
+  };
+  
+  onAnswerItemClick = (name) => {
+    const { clickedBirds, currentBird, score } = this.state;
+    
+    if (currentBird.name === name) {
+      this.setState({
+        guessed: true,
+        score: score + (steps.length - clickedBirds.length - 1)
+      });
+      this.questionAudio.current.pause();
+      this.rightAudio.current.play();
+    } else {
+      this.setState({
+        clickedBirds: [...clickedBirds, name]
+      });
+      this.wrongAudio.current.play();
+    }
+  };
+  
+  render() {
+    const { clickedBirds, currentBird, currentStep, guessed, score, finished } = this.state;
+    const { img, name: currentName, voice } = currentBird;
+    const lastClicked = clickedBirds[clickedBirds.length - 1];
+    const currentBirds = getBirdsByStep(currentStep);
+    const lastClickedInfo = guessed ? currentBird : currentBirds.filter(({ name }) => name === lastClicked)[0];
+    
+    return (
+      <div className='App'>
+        {/* TODO: переместить хэдер в отдельный компонент */}
+        <header>
+          <img className='logoPosition' src={logo} alt='logo'/>
+          <p className='scoreTextColor whiteText'>
+            Score: {score}
+          </p>
+          <div className='header'>
+            {steps.map(({ id, title }) => {
+              const className = `questionListItem whiteText ${currentStep === id ? 'headerActiveColor' : 'headerNotActiveColor'} `;
+              return <span key={id} className={className}>{title}</span>;
+            })}
+          </div>
+        </header>
+        {finished
+          ? (
+            <div className='flex finishBlock'>
+              <div className='whiteText'>
+                Вы набрали {score} баллов из {maxScore} возможных.
+              </div>
+              {score === maxScore
+                ? (
+                  <div>
+                    <img className='padding15 congratsImg' src={congrats} alt='congrats'/>
+                  </div>
+                ) : (
+                  <button className='repeatBtn headerActiveColor marginTop whiteText' onClick={this.repeatClick}>
+                    Начать игру заново
+                  </button>
+                )}
+            </div>
+          ) : (
+            <div>
+              <div className='currentQuestion flex'>
+                <img className='flex padding15 currentBird' src={guessed ? img : unknownBird} alt='current bird'/>
+                <div className='currentBirdBlock'>
+                  <div className='currentBirdName whiteText'>{guessed ? currentName : '******'}</div>
+                  <div>
+                    <audio className='audio' controls ref={this.questionAudio} src={voice}>
+                      {/*<source src={voice} type="audio/mp3"/>*/}
+                    </audio>
+                  </div>
+                </div>
+              </div>
+              <div className='flex'>
+                <div className='marginTop'>
+                  {currentBirds.map(({ name }) => {
+                    const clicked = clickedBirds.includes(name);
+                    
+                    const className = clicked
+                      ? 'wrongAnswerItem'
+                      : guessed && name === currentName ? 'rightAnswerItem' : '';
+                    
+                    const onClick = (guessed || clicked)
+                      ? () => {
+                      }
+                      : () => this.onAnswerItemClick(name);
+                    
+                    return (
+                      <div key={name} className='answerItem whiteText' onClick={onClick}>
+                        <span className={`answerItemCircle ${className}`}> </span>
+                        <span>{name}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className='currentQuestion flex marginTop'>
+                  {clickedBirds.length !== 0 || guessed ?
+                    <div className='clickedBirdInfo'>
+                      <div className='flex'>
+                        <img className='padding15 currentBird' src={lastClickedInfo.img} alt='current bird'/>
+                        <div className='currentBirdBlock'>
+                          <div className='whiteText lastClikedName'>{guessed ? currentName : lastClicked}</div>
+                          <div className='whiteText currentBirdLatName'>{lastClickedInfo.latName}</div>
+                          <div>
+                            <audio className='audio' controls src={lastClickedInfo.voice}/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='padding15'>{lastClickedInfo.description}</div>
+                    </div> : <div className='whiteText width600'> Послушайте плеер. Выберите птицу из списка </div>
+                  }
+                </div>
+                <div>
+                </div>
+                <audio ref={this.rightAudio}>
+                  <source src={rightMP3} type="audio/mp3"/>
+                </audio>
+                <audio ref={this.wrongAudio}>
+                  <source src={wrongMP3} type="audio/mp3"/>
+                </audio>
+              
+              </div>
+              <button
+                className={`nextLevelBtn marginTop whiteText ${guessed ? 'rightAnswerItem' : 'nextLevelBtnDefault'}`}
+                onClick={this.onClick} disabled={!guessed}>Next level
+              </button>
+            </div>
+          )}
+      </div>
+    );
+  }
+}
+
+export default App;
